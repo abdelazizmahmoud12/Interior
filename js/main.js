150a3e0657ab48f91aaa1068379798573ec357fb
abdelazizmahmoud12 <abdelaziz123231@gmail.com>
2023-07-27 00:16:29 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,29 @@
+gsap.from(".box-1",{
+    y:-200,
+    duration: 1,
+})
+
+gsap.from(".box-2",{
+    y:200,
+    duration: 1,
+    opacity: -5   
+})
+
+gsap.from(".box-3",{
+    x:300,
+    duration: 1,
+    opacity: -5
+})
+
+gsap.from(".box-4",{
+    x: -200,
+    duration: 1,
+    opacity: -5
+})
+
+
+gsap.from(".box-5",{
+    y:200,
+    duration: 1,
+    opacity: -5
+})
